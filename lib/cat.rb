d2bd22a80d858d83c21218dc1bf01b38380892ab
Brandon Green <brandon@brandonleeproductions.com>
2018-06-20 00:06:07 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,11 +1,11 @@
 class Cat
 
-  attr_accessor :name, 
-  attr_accessor :mood
+  attr_accessor :name # Cat can't change its name, so reader method is best
+  attr_accessor :mood # Cat CAN change its mood so accessor method is best
 
   def initialize(name)
     @name = name
-    self.new = name
+    @mood = nervous
   end
 
 
